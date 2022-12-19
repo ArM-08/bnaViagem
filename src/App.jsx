8b5c6ccfd0fe56bc88205c8f08bc46479f163330
ArM-08 <62227850+ArM-08@users.noreply.github.com>
2022-12-19 19:48:04 -0300

    error 404

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { ProtectedLayout } from "./Components/ProtectedLayout";
 import Home from "./Pages/Home";
 import Destinos from "./Pages/Destinos";
 import Promocoes from "./Pages/Promocoes";
-import Contato from "./Pages/Contato"
+import Contato from "./Pages/Contato";
 import Cadastro from "./Pages/Cadastro";
 import DashBoard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
@@ -18,11 +18,11 @@ function App() {
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<Home/>} />
-          <Route path="/destinos" element={<Destinos/>}/>
-          <Route path="/promocoes" element={<Promocoes/>}/>
-          <Route path="/contato" element={<Contato/>}/>
-          <Route path="/cadastro" element={<Cadastro/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="destinos" element={<Destinos/>}/>
+          <Route path="promocoes" element={<Promocoes/>}/>
+          <Route path="contato" element={<Contato/>}/>
+          <Route path="cadastro" element={<Cadastro/>}/>
+          <Route path="login" element={<Login/>}/>
           <Route
             path="/profile"
             element={
