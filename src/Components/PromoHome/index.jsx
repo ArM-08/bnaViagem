49c18ed51f696c722675d1b1a9f91eac3f9e83d7
ArM-08 <62227850+ArM-08@users.noreply.github.com>
2022-12-31 15:15:05 -0300

    style PromoHome

--- src/Components/PromoHome/index.jsx
+++ src/Components/PromoHome/index.jsx
@@ -1,45 +1,30 @@
 import { Card, Row } from "antd";
-import PortoGalinha from "./../../assets/images/PortoGalinhasPromo.png";
-import Ceara from "./../../assets/images/CearaPromo.png";
-import Orlando from "./../../assets/images/OrlandoPromo.png";
+import "./index.css";
+
 const PromoHome = () => {
   return (
-    <div style={{ margin: "0 auto", width: "70%" }}>
+    <div className="containerPromo">
       <h1>Pacotes para economizar</h1>
 
       <Row
-        style={{
-          width: "100%",
-          justifyContent: "space-between",
-          gap: "20px",
-        }}
+      className="rowPromo"
+      xl={12}
       >
         <Card
+        className="CardPromo1"
           bordered={false}
           hoverable="true"
-          style={{
-            width: 400,
-            height: 290,
-            backgroundImage: `url(${PortoGalinha})`,
-          }}
+          
         />
         <Card
           bordered={false}
           hoverable="true"
-          style={{
-            width: 400,
-            height: 290,
-            backgroundImage: `url(${Ceara})`,
-          }}
+          className="CardPromo2"
         />
         <Card
           bordered={false}
           hoverable="true"
-          style={{
-            width: 400,
-            height: 290,
-            backgroundImage: `url(${Orlando})`,
-          }}
+          className="CardPromo3"
         />
       </Row>
     </div>
